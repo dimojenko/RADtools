855952f4b96410a80220b376d98d5a5e53b10f52
dimojenko <dmojsejenko@gmail.com>
2023-10-24 16:11:42 -0400

    Troubleshoot cam numbering.

--- imageCapture/imageCapture.py
+++ imageCapture/imageCapture.py
@@ -50,21 +50,21 @@ def captureImage(camNum, fname, fcomp=[cv2.IMWRITE_JPEG_QUALITY, 90], webcam=Tru
 
     """
     # get the path to the user's Camera Roll folder
-    if webcam:
+    if webcam: # laptop
         user = subprocess.run(['cmd.exe', '/c', 'echo %USERNAME%'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
         cameraRoll = "C:\\Users\\" + user + "\\OneDrive - JNJ\\Pictures\\Camera Roll\\"
-    else:
-        cameraRoll = "C:\\Users\\User\\Pictures\\Camera Roll\\"
-    fpath = cameraRoll + fname
-    
-    # image window placement (laptop)
-    x_left = 230
-    x_right = 1190
-    y = 220
-    if webcam: # PC; webcam is camNum 0
+        # image window placement (laptop)
+        x_left = 230
+        x_right = 1190
+        y = 220
+        # adjust camNum to skip index of webcam (0)
         camNum = camNum + 1
+    else: # no webcam == PC
+        cameraRoll = "C:\\Users\\User\\Pictures\\Camera Roll\\"
+        # image window placement (PC)
         x_left = 90
         x_right = 690
+    fpath = cameraRoll + fname
     
     # capture image
     cam = cv2.VideoCapture(camNum, cv2.CAP_DSHOW)
@@ -93,7 +93,7 @@ def captureImage(camNum, fname, fcomp=[cv2.IMWRITE_JPEG_QUALITY, 90], webcam=Tru
             #    - PC screen size: width=1920 height=1080
             cv2.namedWindow(fname)
 
-            if camNum == 2: # left eye
+            if camNum == 2 or (not webcam and camNum == 1): # left eye
                 cv2.moveWindow(fname, x_left, y)
             else: # right eye
                 cv2.moveWindow(fname, x_right, y)
